800b64e9bb3596367910f5ff91fdc1506294cfaf
Carlos Carretero <carloscn90@gmail.com>
2024-09-03 09:52:04 +0200

    refactorización, animación y falta arreglar la carta RocksStone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { NgStyle } from '@angular/common';
-import { Component } from '@angular/core';
-import { push } from '@angular/fire/database';
+import { Component, signal, Signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -15,112 +14,69 @@ export class AppComponent {
 
   idioma: string = "original"; /* original es Inglés */
 
-  /* numero de cartas */
-  evento: number = 36;
-  horda: number = 15;
-  rockStone: number = 20;
-  municion: number = 9;
-
-  /* cartas repetidas */
-  eventoUsadas: number[] = [];
-  hordaUsadas: number[] = [];
-  rockStoneUsadas: number[] = [];
-  municionUsadas: number[] = [];
-
-
+  barajas: any = {
+    evento: { numeroCartas: 36, usadas: [] },
+    horda: { numeroCartas: 15, usadas: [] },
+    rockStone: { numeroCartas: 20, usadas: [] },
+    municion: { numeroCartas: 9, usadas: [] },
+  };
+  
   /* carta en pantalla */
-  rotacion: number = 90;
+  rotacion: number = 0;
   enves: boolean = true;
+  cartaElegida: number = 0;
   tipoDeCarta: string = "evento";
   urlCarta: string = "/cartas/enves/evento.jpeg";
 
-
-  tipo(selecion: string) {
+  elegirBaraja(selecion: string) {
     this.urlCarta = "/cartas/enves/" + selecion + ".jpeg";
     this.tipoDeCarta = selecion;
     this.enves = true;
   }
 
+  sacarCarta() {
+    if (this.enves) {
 
-  resultado() {
-    var numeroDeCartas: number = 0;
-    var resultado: number = 0;
-    var buclefuncionando: boolean = false;
+      this.gestionarCarta(this.tipoDeCarta);
 
-    if (this.enves == true) {
-      switch (this.tipoDeCarta) {
-        case "evento":
-          numeroDeCartas = this.evento;
-          if (this.eventoUsadas.length <= numeroDeCartas - 1) {
-            buclefuncionando = true
-          }
-          else { if (window.confirm("No te quedan cartas de evento ¿Quieres barajar el mazo?")) { this.eventoUsadas = [] } }
-          while (buclefuncionando) {
-            resultado = Math.floor(Math.random() * (numeroDeCartas - 1 + 1)) + 1;
-            if (!this.eventoUsadas.includes(resultado)) {
-              this.eventoUsadas.push(resultado);
-              buclefuncionando = false;
-            }
-          }
-          break;
-        case "horda":
-          numeroDeCartas = this.horda;
-          if (this.hordaUsadas.length <= numeroDeCartas - 1) {
-            buclefuncionando = true
-          }
-          else { if (window.confirm("No te quedan cartas de horda ¿Quieres barajar el mazo?")) { this.hordaUsadas = [] } }
-          while (buclefuncionando) {
-            resultado = Math.floor(Math.random() * (numeroDeCartas - 1 + 1)) + 1;
-            if (!this.hordaUsadas.includes(resultado)) {
-              this.hordaUsadas.push(resultado);
-              buclefuncionando = false;
-            }
-          }
-          break;
-        case "rockstone":
-          numeroDeCartas = this.rockStone;
-          if (this.rockStoneUsadas.length <= numeroDeCartas - 1) {
-            buclefuncionando = true
-          }
-          else { if (window.confirm("No te quedan cartas rockstone ¿Quieres barajar el mazo?")) { this.rockStoneUsadas = [] } }
-          while (buclefuncionando) {
-            resultado = Math.floor(Math.random() * (numeroDeCartas - 1 + 1)) + 1;
-            if (!this.rockStoneUsadas.includes(resultado)) {
-              this.rockStoneUsadas.push(resultado);
-              buclefuncionando = false;
-            }
-          }
-          break;
-        case "municion":
-          numeroDeCartas = this.municion;
-          if (this.municionUsadas.length <= numeroDeCartas - 1) {
-            buclefuncionando = true
-          }
-          else { if (window.confirm("No te quedan cartas de munición ¿Quieres barajar el mazo?")) { this.municionUsadas = [] } }
-          while (buclefuncionando) {
-            resultado = Math.floor(Math.random() * (numeroDeCartas - 1 + 1)) + 1;
-            if (!this.municionUsadas.includes(resultado)) {
-              this.municionUsadas.push(resultado);
-              buclefuncionando = false;
-            }
-          }
-          break;
-        default:
-          window.alert("algo falla");
-      }
-
-      if (resultado != 0) {
-        this.urlCarta = "cartas/" + this.tipoDeCarta + "/" + this.idioma + "/" + resultado + ".jpeg";
-        console.log("cartas/" + this.tipoDeCarta + "/" + this.idioma + "/" + resultado + ".jpeg");
-
+      if (this.cartaElegida > 0) {
+        this.rotacion=1800;
+        setTimeout(() => {
+          this.urlCarta = "cartas/" + this.tipoDeCarta + "/" + this.idioma + "/" + this.cartaElegida + ".jpeg";
+          console.log("cartas/" + this.tipoDeCarta + "/" + this.idioma + "/" + this.cartaElegida + ".jpeg");
+        }, 500);
         this.enves = false;
       }
-      else{this.urlCarta = "/cartas/vacio.jpeg"}
+      else { this.urlCarta = "/cartas/vacio.jpeg" }
     }
     else {
-      this.urlCarta = "/cartas/enves/" + this.tipoDeCarta + ".jpeg"
+      this.rotacion = 0;
+      setTimeout(() => {
+        this.urlCarta = "/cartas/enves/" + this.tipoDeCarta + ".jpeg";
+      }, 150);
       this.enves = true;
     }
 
+  }
+
+  gestionarCarta(tipoCarta:string) {
+    var longitudBaraja = this.barajas[tipoCarta].usadas.length;
+
+    var bucleFuncionando = longitudBaraja < this.barajas[tipoCarta].numeroCartas;
+
+    if (!bucleFuncionando) {
+      if (window.confirm("No te quedan cartas de evento ¿Quieres barajar el mazo?")) {
+        this.barajas[tipoCarta].usadas = [];
+      }
+    }
+
+    while (bucleFuncionando) {
+      this.cartaElegida = Math.floor(Math.random() * (this.barajas[tipoCarta].numeroCartas)) + 1;
+
+      if (!this.barajas[tipoCarta].usadas.includes(this.cartaElegida)) {
+        this.barajas[tipoCarta].usadas.push(this.cartaElegida);
+        bucleFuncionando = false;
+      }
+    }
   }
 }
